345d2ef5d647f0e5799ded269d6fc0b41f8dc865
YogaWerda31 <yogaraya92@gmail.com>
2022-01-04 22:08:03 +0700

    Memperbaiki Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,8 +12,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.black,
-        
-      )
+              )
     );
   }
 }  
@@ -23,84 +24,113 @@ class Login extends StatefulWidget{
 }
 
 class _LoginState extends State<Login>{
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(8),
-        color: Colors.lightBlue,
-        child: Column(
-          children: <Widget> [
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black87,
-                shape: BoxShape.circle,
+  String username = "Yoga";
+  String password = "halo123";
+  String alert = "Siap Login";
+}
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+void prosesLogin() {}
+TextEditingController usernameInput = new TextEditingController();
+TextEditingController passwordInput = new TextEditingController();
 
-              ),
-              child: Center(
-                child: Icon(Icons.person, size:50, color: Colors.white10,),
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    body: Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(8),
+      color: Colors.lightBlue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            decoration:
+            BoxDecoration(color: Colors.black87, shape: BoxShape.circle),
+            child: Center(
+              child: Icon(
+                Icons.person,
+                size: 50,
+                color: Colors.white,
               ),
             ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Selamat Datang di Sports Nutrion, Silahkan Masuk",
+            style: TextStyle(fontSize: 20, color: Colors.black87),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(alert),
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: <Widget>[
+              TextFormField(
+                controller: usernameInput,
+                validator: (value){
+                  if (value.isEmpty){
+                    return "Isi Username Anda";
+                  }
 
-            SizedBox(height: 20,),
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black87)),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      size: 40,
+                    ),
+                    hintText: "Masukkan Username",
+                    hintStyle: TextStyle(color: Colors.black87)),
+                    labelText: "Username",
+                    labelStyle: TextStyle(color: Colors.black87)),
+              ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: passwordInput,
+                        validator: (value){
+                          if (value.isEmpty){
+                            return "Isi Password Anda";
+                          }
 
-            Text("Selamat Datang di Sports Nutrition, Silahkan Login", style: TextStyle(fontSize: 20, color: Colors.black87),),
-
-            SizedBox(height: 20,),
-
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black87)
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black87)),
+                            prefixIcon: Icon(
+                              Icons.lock,size: 40,
+                            ),
+                            hintText: "Masukkan Password",
+                            hintStyle: TextStyle(color: Colors.black87)),
+                            labelText: "Password",
+                            labelStyle: TextStyle(color: Colors.black87)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                prefixIcon: Icon(Icons.person, size: 40,),
-                hintText: "Masukkan Username",
-                hintStyle: TextStyle(color: Colors.black87),
-                labelText: "Username",
-                labelStyle: TextStyle(color: Colors.black87)
-              )
-            ),
-
-            SizedBox(height: 20,),
-
-             TextFormField(
-               obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black87)
-                ),
-                prefixIcon: Icon(Icons.lock, size: 40,),
-                hintText: "Masukkan Password",
-                hintStyle: TextStyle(color: Colors.black87),
-                labelText: "Password",
-                labelStyle: TextStyle(color: Colors.black87)
-              )
-             ),
-
-             SizedBox(height: 20,),
-
-             Card(
-               color: Colors.black87,
-               elevation: 5,
-               child: Container(
-                 height: 50,
-                 child: InkWell(
-                   splashColor: Colors.white,
-                   onTap: () {},
-                   child: Center(
-                     child: Text("Masuk", style: TextStyle(fontSize: 20, color: Colors.white,)),
-                   ),
-                 )
-               ),
-             )
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
       )
-    );
-  }
+    )
+  );
 }
