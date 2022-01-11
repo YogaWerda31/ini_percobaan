e16832764b8b359e7d088e2a24c9a078fb5d06e2
YogaWerda31 <yogaraya92@gmail.com>
2022-01-11 10:34:47 +0700

    Buat Formulir

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,90 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(new MaterialApp(
+    home: new Home(),
+  ));
+}
 
-class MyApp extends StatelessWidget{
+class Home extends StatefulWidget{
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Home',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.black,
-              )
-    );
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _jk="";
+
+void _pilihjk(String value){
+    setState(() {
+      _jk=value;
+    });
   }
-}  
-
-//Login Page//
-class Login extends StatefulWidget{
   @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login>{
-  String username = "Yoga";
-  String password = "halo123";
-  String alert = "Siap Login";
-}
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-void prosesLogin() {}
-TextEditingController usernameInput = new TextEditingController();
-TextEditingController passwordInput = new TextEditingController();
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new Icon(Icons.list),
+        title: new Text("Formulir"),
+        backgroundColor: Colors.teal,
 
 
-@override
-Widget build(BuildContext context){
-  return Scaffold(
-    body: Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.all(8),
-      color: Colors.lightBlue,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            width: 100,
-            height: 100,
-            decoration:
-            BoxDecoration(color: Colors.black87, shape: BoxShape.circle),
-            child: Center(
-              child: Icon(
-                Icons.person,
-                size: 50,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Selamat Datang di Sports Nutrion, Silahkan Masuk",
-            style: TextStyle(fontSize: 20, color: Colors.black87),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(alert),
-          SizedBox(
-            height: 20,
-          ),
-          Column(
-            children: <Widget>[
-              TextFormField(
-                controller: usernameInput,
-                validator: (value){
-                  if (value.isEmpty){
-                    return "Isi Username Anda";
-                  }
+      ),
 
-                  return null;
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black87)),
-                    prefixIcon: Icon(
-                      Icons.person,
-                      size: 40,
-                    ),
-                    hintText: "Masukkan Username",
-                    hintStyle: TextStyle(color: Colors.black87)),
-                    labelText: "Username",
-                    labelStyle: TextStyle(color: Colors.black87)),
-              ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      TextFormField(
-                        controller: passwordInput,
-                        validator: (value){
-                          if (value.isEmpty){
-                            return "Isi Password Anda";
-                          }
-
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black87)),
-                            prefixIcon: Icon(
-                              Icons.lock,size: 40,
-                            ),
-                            hintText: "Masukkan Password",
-                            hintStyle: TextStyle(color: Colors.black87)),
-                            labelText: "Password",
-                            labelStyle: TextStyle(color: Colors.black87)),
-                          ),
-                        ),
-                      ),
-                    ],
+      body: ListView(
+        children: [
+          new Container(
+            padding: new EdgeInsets.all(10.0),
+            child: new Column(
+              children: <Widget>[
+                new TextField(
+                  decoration: new InputDecoration(
+                    hintText: "Nama Lengkap",
+                    labelText: "Nama Lengkap",
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(20.0),
+                    )
                   ),
                 ),
-              ),
-            ],
-          )
+
+                new Padding(padding: new EdgeInsets.only(top: 20.0),),
+                new TextField(
+                  obscureText: true,
+                  decoration: new InputDecoration(
+                    hintText: "Password",
+                    labelText: "Password",
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(20.0),
+                    )
+                  ),
+                ),
+                new Padding(padding: new EdgeInsets.only(top: 20.0),),
+                new TextField(
+                  maxLines: 3,
+                  decoration: new InputDecoration(
+                    hintText: "Deskripsi",
+                    labelText: "Deskripsi",
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(20.0),
+                    )
+                  ),
+                ),
+                new Padding(padding: new EdgeInsets.only(top: 20.0),),
+
+                new RadioListTile(
+                  value: "Laki - Laki",
+                  title: new Text("Laki - Laki"),
+                  groupValue: _jk,
+                  onChanged: (String value){
+                    _pilihjk(value);
+                  },
+                  
+                )
+              ],
+            ),
+          ),
         ],
-      )
-    )
-  );
+      ),
+    );
+  }
 }
