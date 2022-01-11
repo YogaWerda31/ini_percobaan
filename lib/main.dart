d3101364ba3e0b00133ad17759b0f3a93817e4c2
YogaWerda31 <yogaraya92@gmail.com>
2022-01-11 18:13:16 +0700

    Formulir hampir jadi

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,40 @@ class Home extends StatefulWidget{
 
 class _HomeState extends State<Home> {
 
+
+  List<String> agama=["Islam","Kristen","Katolik","Hindu","Budha"];
+  String _agama="Islam";
+
+
   String _jk="";
+
+
 
 
 void _pilihjk(String value){
   setState(() {
     _jk=value;
   });
-}  
+}
+
+void _pilihAgama(String value){
+  setState(() {
+    _agama=value;
+  });
+}
+
+void kirimdata(){
+  AlertDialog alertDialog = new AlertDialog(
+    content: new Container(
+      height: 200.0,
+      child: new Column(
+        children: <Widget>[
+          new Text("Nama Lengkap"),
+        ],
+      ),
+    ),
+  ),
+}
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -63,8 +89,8 @@ void _pilihjk(String value){
                 new TextField(
                   maxLines: 3,
                   decoration: new InputDecoration(
-                    hintText: "Deskripsi",
-                    labelText: "Deskripsi",
+                    hintText: "Alamat",
+                    labelText: "Alamat",
                     border: new OutlineInputBorder(
                       borderRadius: new BorderRadius.circular(20.0),
                     )
@@ -76,27 +102,56 @@ void _pilihjk(String value){
                   value: "Laki - Laki",
                   title: new Text("Laki - Laki"),
                   groupValue: _jk,
-                  onChanged:(String value){
-                    _pilihjk(value);
+                  onChanged:(String?value){
+                    _pilihjk(value!);
                   },
                   activeColor: Colors.red,
+                  subtitle: new Text("Pilih ini jika Anda Laki - Laki"),
 
                 ),
                  new RadioListTile(
                   value: "Perempuan",
                   title: new Text("Perempuan"),
                   groupValue: _jk,
-                  onChanged:(String value){
-                    _pilihjk(value);
+                  onChanged:(String?value){
+                    _pilihjk(value!);
                   },
                   activeColor: Colors.red,
+                  subtitle: new Text("Pilih ini jika Anda Perempuan"),
                   
                 ),
+
+                new Padding(padding: new EdgeInsets.only(top: 20.0),),
+
+                new Row(
+                  children: <Widget>[
+                    new Text("Agama", style: new TextStyle(fontSize: 20.0, color: Colors.blue),),
+                    new DropdownButton(
+                onChanged: (String?value){
+                        _pilihAgama(value!);
+                      },
+                      value: _agama,
+                      items: agama.map((String value){
+                        return new DropdownMenuItem(
+                          value: value,
+                          child: new Text(value),
+                        );
+                      }).toList(),
+                    )
+                  ],
+                ),
+
+                new RaisedButton(
+                  child: new Text("Ok"),
+                  color: Colors.red,
+                  onPressed: (){kirimdata ();},
+                )
+
               ],
             ),
           ),
         ],
-      ),
+      )
     );
   }
 }
